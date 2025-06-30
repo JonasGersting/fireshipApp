43773b0ecb5b4c87b2ddff21faa710fcdb72445b
Jonas Gersting <johndoe@example.com>
2025-06-30 21:37:01 +0200

    login page started

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,5 @@
-<script lang="ts">
-    import type { Snippet } from 'svelte';
-    import type { LayoutData } from './$types';
-
-    let { data, children }: { data: LayoutData, children: Snippet } = $props();
-    import "../app.css";
+<script>
+  let { children } = $props();
+  import "../app.css";
 </script>
-
 {@render children()}
